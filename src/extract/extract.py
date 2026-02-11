486d66ad88904ae1060e508d7a40da6b24476ec5
Jose Griffiths <79573468+josegriff@users.noreply.github.com>
2026-02-11 05:17:43 +0000

    Implement extract and transform steps

---
+++ src/extract/extract.py
@@ -0,0 +1,44 @@
+from utils.logger import logger
+import pandas as pd
+
+required_columns = [
+    "trans_num",
+    "trans_date_trans_time",
+    "amt",
+    "merchant",
+    "category",
+    "is_fraud"
+]
+
+def extract_transactions(input_path: str) -> pd.DataFrame:
+    logger.info(f"[EXTRACT] Reading CSV from {input_path}")
+    
+    try: 
+        df = pd.read_csv(input_path)
+    except Exception as e:
+        logger.error(f"Error reading CSV: {e}")
+        raise
+    
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+    
+    logger.info(f"[EXTRACT] Columns after normalisation: {list(df.columns)}")
+    
+    missing = [col for col in required_columns if col not in df.columns]
+    if missing:
+        logger.error(f"[EXTRACT] Missing required columns: {missing}")
+        raise ValueError(f"Missing required columns: {missing}")
+        
+    extra = [col for col in df.columns if col not in required_columns]
+    if extra:
+        logger.warning(f"[EXTRACT] Extra columns detected: {extra}")
+    
+    df = df[required_columns + extra]
+    
+    logger.info(f"[EXTRACT] Successfully extracted {len(df)} rows")
+    return df
+    
---
+++ src/transform/transform.py
@@ -0,0 +1,7 @@
+from utils.logger import logger
+import pandas as pd
+
+def apply_business_rules(df: pd.DataFrame) -> pd.DataFrame:
+    logger.info("[TRANSFORM] Applying fraud rules (placeholder)")
+    
+    return df
